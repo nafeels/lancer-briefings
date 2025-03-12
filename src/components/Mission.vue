998d6a158b0d33de5702821bdc8f46f16bf5409a
nafeels <nawb3r@gmail.com>
2025-03-11 21:47:38 -0300

    Update Mission.vue

--- src/components/Mission.vue
+++ src/components/Mission.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="mission" :class="[{ active: isActive }, this.mission.status]">
 		<div class="name">
-			<h1>Mission // {{ mission.slug }}</h1>
+			<h1>Missão // {{ mission.slug }}</h1>
 			<h2>{{ mission.name }}</h2>
 		</div>
 		<div class="status" :class="this.mission.status">
