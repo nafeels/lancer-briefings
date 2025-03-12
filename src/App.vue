ff123d05402732f17683be1d6e228bd7987aeae1
nafeels <nawb3r@gmail.com>
2025-03-11 23:25:59 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "A Descida",
           "status": "start"
         },
       ],
